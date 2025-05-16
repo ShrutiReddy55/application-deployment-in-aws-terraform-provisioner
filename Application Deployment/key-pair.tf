0670f14d8efee7943f24c6fed81ca98953fe8369
Shruti Reddy <shruthireddy1001@gmail.com>
2025-05-16 15:31:45 +0530

    Update key-pair.tf

--- Application Deployment/key-pair.tf
+++ Application Deployment/key-pair.tf
@@ -1,4 +1,4 @@
-resource "aws_key_pair" "example" {
+resource "aws_key_pair" "shrukey" {
   key_name   = "keypair" 
   public_key = file("/root/.ssh/id_ed25519.pub")  # Replace the path for your public key file
 }
