6406639ab7be40e4e84f582371c33057d9722aa5
Shruti Reddy <shruthireddy1001@gmail.com>
2025-05-16 14:33:23 +0530

    Update route-table-association.tf

--- Application Deployment/route-table-association.tf
+++ Application Deployment/route-table-association.tf
@@ -1,4 +1,4 @@
-resource "aws_route_table_association" "rta1" {
+resource "aws_route_table_association" "myrt01" {
   subnet_id      = aws_subnet.sub1.id
   route_table_id = aws_route_table.RT.id
 }
