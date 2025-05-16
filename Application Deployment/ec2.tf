a7e020a92e5400b8e89836bb3ef1660963ee47b4
Shruti Reddy <shruthireddy1001@gmail.com>
2025-05-16 15:31:22 +0530

    Update ec2.tf

--- Application Deployment/ec2.tf
+++ Application Deployment/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "myserver" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
-  key_name               = aws_key_pair.example.key_name
+  key_name               = aws_key_pair.shrukey.key_name
   vpc_security_group_ids = [aws_security_group.webSg.id]
   subnet_id              = aws_subnet.sub1.id
 
@@ -14,7 +14,7 @@ resource "aws_instance" "myserver" {
 
    # File provisioner to copy a file from local to the remote EC2 instance
    provisioner "file" {
-    source      = "../Flask Application/app.py"  # Replace with the path to your local file
+    source      = "../Flaskapplication/app.py"  # Replace with the path to your local file
     destination = "/root/assignment/Flaskapplication/app.py"  # Replace with the path on the remote instance
    }
 
