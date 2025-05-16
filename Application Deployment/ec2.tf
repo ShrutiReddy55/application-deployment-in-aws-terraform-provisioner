462bcdd1b54f3fda81fa044822f5a7c0e0b6c8e0
Shruti Reddy <shruthireddy1001@gmail.com>
2025-05-16 14:32:01 +0530

    Update ec2.tf

--- Application Deployment/ec2.tf
+++ Application Deployment/ec2.tf
@@ -1,21 +1,21 @@
 resource "aws_instance" "myserver" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
-  key_name      = aws_key_pair.example.key_name
+  key_name               = aws_key_pair.example.key_name
   vpc_security_group_ids = [aws_security_group.webSg.id]
   subnet_id              = aws_subnet.sub1.id
 
   connection {
     type        = "ssh"
     user        = "ubuntu"  # Replace with the appropriate username for your EC2 instance
-    private_key = keypair("/c/Users/Mathesh M/.ssh/id_rsa")  # Replace with the path to your private key
+    private_key = keypair("/root/.ssh/id_rsa")  # Replace with the path to your private key
     host        = self.public_ip
    }
 
    # File provisioner to copy a file from local to the remote EC2 instance
    provisioner "file" {
     source      = "../Flask Application/app.py"  # Replace with the path to your local file
-    destination = "/home/ubuntu/app.py"  # Replace with the path on the remote instance
+    destination = "/root/assignment/Flaskapplication/app.py"  # Replace with the path on the remote instance
    }
 
    provisioner "remote-exec" {
